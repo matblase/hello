338d0b97c68525214d643a1dba8ea789fd37e6eb
matblase <mat20007@byui.edu>
2026-04-25 19:24:07 -0600

    updated my js and css and html and other files

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImg = document.querySelector(".hero-banner img");
+heroImg.src = parkData.images[0].url;
+heroImg.alt = parkData.images[0].altText;
+
+function parkInfoTemplate(info) {
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
+}
+
+const heroContent = document.querySelector(".hero-banner__content");
+heroContent.innerHTML = parkInfoTemplate(parkData);
